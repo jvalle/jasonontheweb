845e6c9d359a128a6a103b2c6bb61c20aeeb1a5e
Jason <jcvalle@mail.usf.edu>
2013-10-24 14:18:44 -0400

    barebones login methods and layout

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
+var UserProvider = require('./users/user-provider').UserProvider;
 
 var app = module.exports = express();
 
@@ -21,7 +22,8 @@ app.configure('production', function () {
 	app.use(express.errorHandler());
 });
 
-var articleProvider = new ArticleProvider('localhost',27017);
+var articleProvider = new ArticleProvider('localhost', 27017);
+var userProvider = new UserProvider('localhost', 27017);
 
 app.get('/', function (req, res) {
 	articleProvider.findAll(function (error, docs) {
@@ -29,6 +31,23 @@ app.get('/', function (req, res) {
 			title: 'jason on the web',
 			articles: docs
 		});
+	});
+});
+
+app.get('/login', function (req, res) {
+	res.render('login.jade');
+});
+
+app.post('/login', function (req, res){
+	userProvider.auth({
+		username: req.param('username'),
+		password: req.param('password')
+	}, function (error, loggedIn) {
+		if (loggedIn) {
+			res.redirect('/blog/new');
+		} else {
+			res.redirect('/');
+		}
 	});
 });
 
@@ -61,3 +80,4 @@ app.get('/work', function (req, res) {
 });
 
 app.listen(3000);
+console.log("App.js initiated on port: " + 3000);
