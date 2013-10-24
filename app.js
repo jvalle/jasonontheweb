f1520fe5f3fb7919ff6730e8a42069007818420d
Jason <jcvalle@mail.usf.edu>
2013-10-24 11:55:02 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
+
+var app = module.exports = express();
+
+app.configure(function () {
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(require('stylus').middleware({src: __dirname + '/public'}));
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function () {
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function () {
+	app.use(express.errorHandler());
+});
+
+var articleProvider = new ArticleProvider('localhost',27017);
+
+app.get('/', function (req, res) {
+	articleProvider.findAll(function (error, docs) {
+		res.render('index.jade', {
+			title: 'jason on the web',
+			articles: docs
+		});
+	});
+});
+
+app.get('/blog/new', function (req, res) {
+	res.render('blog_new.jade', {
+		title: 'New Post'
+	});
+});
+
+app.post('/blog/new', function (req, res) {
+	articleProvider.save({
+		title: req.param('title'),
+		body: req.param('body'),
+		postUrl: req.param('postUrl')
+	}, function (error, docs) {
+		res.redirect('/');
+	});
+});
+
+app.get('/blog/:postUrl', function (req, res) {
+	articleProvider.findById(req.params.postUrl, function (error, article) {
+		res.render('blog_article.jade',  {
+			article: article
+		});
+	});	
+});
+
+app.get('/work', function (req, res) {
+	res.render('work.jade');
+});
+
+app.listen(3000);
---
+++ articleprovider-mongodb.js
@@ -0,0 +1,68 @@
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+var BSON = require('mongodb').BSON;
+var ObjectID = require('mongodb').ObjectID;
+
+ArticleProvider = function(host, port) {
+  this.db= new Db('dev-blog-db', new Server(host, port, {auto_reconnect: true}, {}), {safe: true});
+  this.db.open(function(){});
+};
+
+
+ArticleProvider.prototype.getCollection= function(callback) {
+  this.db.collection('articles', function(error, article_collection) {
+    if( error ) callback(error);
+    else callback(null, article_collection);
+  });
+};
+
+ArticleProvider.prototype.findAll = function(callback) {
+    this.getCollection(function(error, article_collection) {
+      if( error ) callback(error)
+      else {
+        article_collection.find().sort({created_at: -1}).toArray(function(error, results) {
+          if( error ) callback(error)
+          else callback(null, results)
+        });
+      }
+    });
+};
+
+
+ArticleProvider.prototype.findById = function(postUrl, callback) {
+    this.getCollection(function(error, article_collection) {
+      if( error ) callback(error)
+      else {
+        article_collection.findOne({postUrl: postUrl}, function(error, result) {
+          if( error ) callback(error)
+          else callback(null, result)
+        });
+      }
+    });
+};
+
+ArticleProvider.prototype.save = function(articles, callback) {
+    this.getCollection(function(error, article_collection) {
+      if( error ) callback(error)
+      else {
+        if( typeof(articles.length)=="undefined")
+          articles = [articles];
+
+        for( var i =0;i< articles.length;i++ ) {
+          article = articles[i];
+          article.created_at = new Date();
+          if( article.comments === undefined ) article.comments = [];
+          for(var j =0;j< article.comments.length; j++) {
+            article.comments[j].created_at = new Date();
+          }
+        }
+
+        article_collection.insert(articles, function() {
+          callback(null, articles);
+        });
+      }
+    });
+};
+
+exports.ArticleProvider = ArticleProvider;
