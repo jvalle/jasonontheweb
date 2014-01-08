e9937df37e32155bd8e4ff0616dc4bee4f21be97
jvalle <jcvalle@mail.usf.edu>
2014-01-08 18:26:23 -0500

    rewrote article insert into database for one at a time

--- articleprovider-mongodb.js
+++ articleprovider-mongodb.js
@@ -5,7 +5,7 @@ var BSON = require('mongodb').BSON;
 var ObjectID = require('mongodb').ObjectID;
 
 ArticleProvider = function(host, port) {
-  this.db= new Db('dev-blog-db', new Server(host, port, {auto_reconnect: true}, {}), {safe: true});
+  this.db = new Db('dev-blog-db', new Server(host, port, {auto_reconnect: true}, {}), {safe: true});
   this.db.open(function(){});
 };
 
@@ -42,26 +42,41 @@ ArticleProvider.prototype.findById = function(postUrl, callback) {
     });
 };
 
-ArticleProvider.prototype.save = function(articles, callback) {
-    this.getCollection(function(error, article_collection) {
-      if( error ) callback(error)
-      else {
-        if( typeof(articles.length)=="undefined")
-          articles = [articles];
+// ArticleProvider.prototype.save = function(articles, callback) {
+//     this.getCollection(function(error, article_collection) {
+//       if( error ) callback(error)
+//       else {
+//         if( typeof(articles.length)=="undefined")
+//           articles = [articles];
 
-        for( var i =0;i< articles.length;i++ ) {
-          article = articles[i];
-          article.created_at = new Date();
-          if( article.comments === undefined ) article.comments = [];
-          for(var j =0;j< article.comments.length; j++) {
-            article.comments[j].created_at = new Date();
-          }
+//         for( var i =0;i< articles.length;i++ ) {
+//           article = articles[i];
+//           article.created_at = new Date();
+//           if( article.comments === undefined ) article.comments = [];
+//           for(var j =0;j< article.comments.length; j++) {
+//             article.comments[j].created_at = new Date();
+//           }
+//         }
+
+//         article_collection.insert(articles, function() {
+//           callback(null, articles);
+//         });
+//       }
+//     });
+// };
+
+ArticleProvider.prototype.save = function(article, callback) {
+    this.getCollection(function (error, article_collection) {
+        if (error) {
+            callback(error);
         }
+        else {
+            article.created_at = new Date();
 
-        article_collection.insert(articles, function() {
-          callback(null, articles);
-        });
-      }
+            article_collection.insert(article, function () {
+                callback(null, article);
+            });
+        }
     });
 };
 
